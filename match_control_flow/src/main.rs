1963e8f5852fe5ef3176d4bc40f6945b1edcb2aa
Jean-Marcel Belmont <jbelmont@foodlogiq.com>
2019-03-03 10:44:50 -0500

    Add enum and match.

---
+++ match_control_flow/src/main.rs
@@ -0,0 +1,47 @@
+#[derive(Debug)]
+enum Coin {
+    Penny,
+    Nickel,
+    Dime,
+    Quarter(UsState),
+}
+
+#[derive(Debug)]
+enum UsState {
+    Alabama,
+    Alaska,
+    // --- more
+}
+
+fn value_in_cents(coin: Coin) -> u32 {
+    match coin {
+        Coin::Penny => 1,
+        Coin::Nickel => 5,
+        Coin::Dime => 10,
+        Coin::Quarter(state) => {
+            println!("State quarter from {:?}!", state);
+            25
+        },
+    }
+}
+
+fn plus_one(x: Option<i32>) -> Option<i32> {
+    match x {
+        None => None,
+        Some(i) => Some(i + 1),
+    }
+}
+
+fn main() {
+    let five = Some(5);
+    let size = plus_one(five);
+    let none = plus_one(None);
+
+    let mut count = 0;
+    let coin = Coin::Quarter(UsState::Alabama);
+    if let Coin::Quarter(UsState::Alabama) = coin {
+        println!("State quarter from {:?}!", coin);
+    } else {
+        count += 1;
+    }
+}
