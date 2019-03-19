80e7ff548df21ecb180b7c13d75bb59848a0f6a8
Jean-Marcel Belmont <jbelmont@foodlogiq.com>
2019-03-19 17:44:56 -0400

    Add chapter 15 example.s

---
+++ destructor/src/main.rs
@@ -0,0 +1,24 @@
+struct CustomSmartPointer {
+    data: String,
+}
+
+impl Drop for CustomSmartPointer {
+    fn drop(&mut self) {
+        println!("Dropping CustomSmartPointer with data `{}`!", self.data);
+    }
+}
+
+fn main() {
+    let c = CustomSmartPointer {
+        data: String::from("my stuff")
+    };
+
+    println!("CustomSmartPointer created");
+
+    std::mem::drop(c);
+
+    let d = CustomSmartPointer {
+        data: String::from("other stuff")
+    };
+    println!("CustomSmartPointers created. {}", d.data);
+}
