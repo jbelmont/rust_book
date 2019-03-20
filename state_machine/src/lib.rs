75289f2a867caa87e79aa780b9cf1810a8ac1f03
Jean-Marcel Belmont <jbelmont@foodlogiq.com>
2019-03-20 18:55:16 -0400

    Add examples for chapter 17.

---
+++ state_machine/src/lib.rs
@@ -0,0 +1,37 @@
+
+#![allow(unused_variables)]
+fn main() {
+pub struct Post {
+    state: Option<Box<dyn State>>,
+    content: String,
+}
+
+impl Post {
+    // --snip--
+    pub fn request_review(&mut self) {
+        if let Some(s) = self.state.take() {
+            self.state = Some(s.request_review())
+        }
+    }
+}
+
+trait State {
+    fn request_review(self: Box<Self>) -> Box<dyn State>;
+}
+
+struct Draft {}
+
+impl State for Draft {
+    fn request_review(self: Box<Self>) -> Box<dyn State> {
+        Box::new(PendingReview {})
+    }
+}
+
+struct PendingReview {}
+
+impl State for PendingReview {
+    fn request_review(self: Box<Self>) -> Box<dyn State> {
+        self
+    }
+}
+}
---
+++ trait_objects/src/main.rs
@@ -0,0 +1,1 @@
+use trait_objects::Draw;
